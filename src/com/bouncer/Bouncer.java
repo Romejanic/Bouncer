86f5cbf5f349ec2220337d031443147a70d3747a
Jack Davenport <jack.davenport5011@gmail.com>
2020-05-25 14:07:22 +1000

    Added bStats metrics

--- src/com/bouncer/Bouncer.java
+++ src/com/bouncer/Bouncer.java
@@ -3,14 +3,21 @@ package com.bouncer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.bouncer.cmd.CommandBouncer;
+import com.bouncer.util.Metrics;
 
 public class Bouncer extends JavaPlugin {
 
+	private static final int BSTATS_PLUGIN_ID = 7644;
+	
 	private static Bouncer instance;
+	
+	@SuppressWarnings("unused")
+	private Metrics metrics;
 	
 	@Override
 	public void onEnable() {
 		instance = this;
+		this.metrics = new Metrics(this, BSTATS_PLUGIN_ID);
 		getCommand("bouncer").setExecutor(new CommandBouncer(this));
 		
 		getLogger().info("Started up Bouncer!");
